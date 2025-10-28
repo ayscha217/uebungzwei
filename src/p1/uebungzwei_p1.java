d6a3b6758c921fe29773765fbc504cfa57c19832
ayscha217 <abersano@smail.uni-koeln.de>
2025-10-28 18:18:01 +0100

    uebungzwei

---
+++ src/p1/uebungzwei_p1.java
@@ -0,0 +1,15 @@
+package p1;
+
+import java.util.Scanner;
+
+public class uebungzwei_p1 {
+
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in) ;
+	String zeile = new String ();
+	zeile = s.nextLine();
+	System.out.println(zeile);
+	s.close ();
+	
+}}
+
